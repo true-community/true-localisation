0bb46756c13021b729cf57f7eb4a88a282f3ae93
Lehrax <lehrax@gmail.com>
2023-12-29 13:46:14 +0200

    Added translation files

---
+++ .dbtool/utils/db_pack.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# Requirements: xmllint, jq, tofrodos
+
+content_type=$1
+[ -z ${content_type} ] && exit 1
+[ ! -d ".dbtool/extracted/${content_type}/" ] && exit 1
+
+rm -r .dbtool/minified/${content_type}/* 2> /dev/null || mkdir -p .dbtool/minified/${content_type}/
+
+for archive_name in $(ls -1v .dbtool/extracted/${content_type}/); do
+
+  echo
+  echo ${archive_name}
+
+  # move files into the right archive
+  for file in $(jq -r "to_entries[] | select(.value == \"${archive_name}\") | .key" .dbtool/extracted/${content_type}/.index.json); do
+    # uglify
+    direct_file=".dbtool/minified/${content_type}/${file}"
+    mkdir -p $(dirname ${direct_file})
+    cp gamedata/${file} ${direct_file}.tmp
+    xmllint --noblanks ${direct_file}.tmp > ${direct_file} 2> /dev/null
+    exit_code=$?
+    if [ "$exit_code" -ne "0" ]; then
+      rm ${file} 2> /dev/null
+    else
+      mkdir -p .dbtool/separated/${content_type}/${archive_name}/$(dirname ${file})
+      cp ${direct_file} .dbtool/separated/${content_type}/${archive_name}/${file}
+    fi
+
+    rm ${direct_file}.tmp 2> /dev/null
+  done
+
+  find . -type f -name "*.xml" -print0 | xargs -0 todos
+
+  if [ -d ".dbtool/separated/${content_type}/${archive_name}" ]; then
+    # .dbtool/db_converter/build/src/db_converter --pack --xdb ${dir_name} --out ./content/${content_type}/$(basename ${dir_name})$(date +%s).db 2> /dev/null
+    wine ./converter.exe -pack -xdb .dbtool/separated/${content_type}/${archive_name} -out ./content/${content_type}/$(basename $(echo ${archive_name} | sed "s/\./$(date +%s)\./")) #2> /dev/null
+  fi
+done
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# true-localisation
+# true-localisation | True Stalker
+
+## Translation flow
+
+1. Make edits in lines
+2. Test changes in game
+
+
+## (optionally) Extract DB entries yourself
+
+0. Make sure you have a suitable converter tool at hand (ex: [CoC DB converter](https://www.moddb.com/mods/call-of-chernobyl/downloads/cop-coc-db-converter)), place `converter.exe` in this directory next to _README.md_ file
+1. Place up-to-date localisation file into content/resources directory
+2. Run the `./run.sh unpack` to extract strings
+
+## (optionally) Create DB files yourself [**WIP!**]
+
+0. Make sure you have a suitable converter tool at hand (ex: [CoC DB converter](https://www.moddb.com/mods/call-of-chernobyl/downloads/cop-coc-db-converter)), place `converter.exe` in this directory next to _README.md_ file
+1. Create the DB files via `./run.sh pack`
